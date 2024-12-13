fe4fd53676d6c721eb5e538ebbdf76e33dd6c80e
Chris Olivier <cjolivier01@apache.org>
2024-12-13 09:21:39 -0800

    ..

--- src/apps/run.sh
+++ src/apps/run.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+SCRIPT_DIR=$(dirname $(realpath $0))
+
 APP_FILE="$1"
-MANIFEST_FILE="/tmp/ds.$(basename ${APP_FILE} .yaml)/manifest.yaml"
+APP_NAME="$(basename ${APP_FILE} .yaml)"
+#RUNDIR="/tmp"
+RUNDIR="${SCRIPT_DIR}/../../rundir"
+MANIFEST_FILE="${RUNDIR}/ds.${APP_NAME}/manifest.yaml"
+
 echo "MANIFEST_FILE=${MANIFEST_FILE}"
 
 #if [ ! -f "${MANIFEST_FILE}" ]; then
-RUN_POSTFIX="-run=false"  execute_graph.sh --fresh-manifest "${APP_FILE}" 
+RUN_POSTFIX="-run=false"  execute_graph.sh -a "${RUNDIR}" --fresh-manifest "${APP_FILE}" 
 #fi
 
 GRAPH_FILES="${APP_FILE}" \
